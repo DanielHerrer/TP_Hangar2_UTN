9d556e8d4a5da5f0a7b791765896d6a19cedd881
Castro Lautaro <castrolautaro235@gmail.com>
2024-10-24 20:32:22 -0300

    Agregando metodos para trabajar con la clase Avion. Creando ENUM de Rangos

---
+++ demo/src/main/java/control/ControlAviones.java
@@ -0,0 +1,158 @@
+package control;
+
+import entidades.Avion;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ControlAviones {
+
+    ArrayList<Avion> listaAviones;
+
+
+    /**
+     * @return Muestra a todos los aviones que estan dados de alta
+     */
+    public String mostrarAvionesDadosAlta () {
+
+        StringBuilder sb = new StringBuilder("");
+
+        for (Avion avi : listaAviones) {
+            if (avi.getAlta() == 1) {
+                sb.append(avi.toString()).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @return Muestra a los aviones que estan dados de baja
+     */
+    public String mostrarAvionesDadosBaja () {
+
+        StringBuilder sb = new StringBuilder("");
+
+        for (Avion avi : listaAviones) {
+            if (avi.getAlta() == 0) {
+                sb.append(avi.toString()).append("\n");
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @return Muestra a todos los aviones del sistema
+     */
+    public String mostrarAviones () {
+        StringBuilder sb = new StringBuilder("");
+
+        for (Avion avi : listaAviones) {
+           sb.append(avi.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     *
+     * @return Muestra a los aviones pero destacando el nivel de combustible que tienen
+     */
+    public String mostrarNivelCombustible () {
+        StringBuilder sb = new StringBuilder("");
+
+        for (Avion avi : listaAviones) {
+            sb.append(avi.getId()).append(" ").append(avi.getCombustibleActual()).append(" ").append(avi.getNombre()).
+            append(" ").append(avi.getModelo()).append(" ").append(avi.getAerolinea()).append("\n");
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     *
+     * @param nombre recibe el nombre o una subcadena del avion que se desea encontrar
+     * @return devuelve un String con los aviones cuyo nombre coincide
+     */
+    public String buscarAvionPorNombre (String nombre) {
+        StringBuilder sb = new StringBuilder("");
+        boolean encontrado = false;
+
+        for (Avion avi : listaAviones) {
+            if (avi.getNombre().toLowerCase().contains(nombre.toLowerCase())) {
+                sb.append(avi.toString()).append("\n");
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            return "No hay coincidencias con el nombre ingresado \n";
+        }
+        return sb.toString();
+    }
+
+    /**
+     * @see "Usa un arreglo auxiliar para no modificar le original"
+     * @return retorna un arreglo auxiliar ordenado alfabeticamente
+     */
+    public String mostrarAvionesAlfabetico () {
+        ArrayList<Avion> auxiliar = new ArrayList<>(listaAviones);
+        Collections.sort(auxiliar);
+        StringBuilder sb = new StringBuilder("");
+        for (Avion avi : auxiliar) {
+            sb.append(avi.toString()).append("\n");
+        }
+        return sb.toString();
+    }
+
+
+    /**
+     * @see "Modifica la baja/alta de el avion elegido"
+     * @param id recibe el id del avion que desea modificar (el paramatro deberia venir desde la vista)
+     */
+    public int modificarAltaAvion (int id) {
+        Avion avion = null;
+
+        for (Avion avi : listaAviones) {
+            if (id == avi.getId()) {
+                if (avi.getAlta() == 1) {
+                    avi.setAlta(0);
+                    avion = avi;
+                }
+                else {
+                    avi.setAlta(1);
+                    avion = avi;
+                }
+            }
+            else {
+                return -1;
+                //retorno -1 si el id es incorrecto
+            }
+        }
+        return avion.getAlta();
+    }
+
+    /**
+     * @see "Si el combustible de un avion es menor a 80 lo carga hasta el maximo"
+     * @return devuelve un mensaje de acuerdo a si se cargaron o no los aviones
+     */
+    public String cargarCombustible () {
+        boolean cargas = false;
+
+        for (Avion avi : listaAviones) {
+            if (avi.getCombustibleActual() < 80) {
+                avi.setCombustibleActual(100);
+                cargas = true;
+            }
+        }
+        if (cargas) {
+            return "Se lleno el tanque de todos los aviones \n";
+        }
+        else {
+            return "Los aviones ya tienen el tanque lleno \n";
+        }
+    }
+
+    //QUEDAN POR HACER LAS FUNCIONES DE MODIFICACION
+
+}
--- demo/src/main/java/entidades/Piloto.java
+++ demo/src/main/java/entidades/Piloto.java
@@ -1,6 +1,7 @@
 package entidades;
 
 import enums.Genero;
+import enums.Rango;
 
 import java.util.Objects;
 
@@ -8,13 +9,13 @@ public class Piloto extends Persona {
 
     private String numeroLicencia;
     private int horasVuelo;
-    private String rango;
+    private Rango rango;
 
-    public Piloto(String dni, String nombreApellido, Genero genero, int anioNacimiento, String numeroLicencia, String rango) {
+    public Piloto(String dni, String nombreApellido, Genero genero, int anioNacimiento, String numeroLicencia) {
         super(dni, nombreApellido, genero, anioNacimiento);
         this.numeroLicencia = numeroLicencia;
         this.horasVuelo = 0;
-        this.rango = rango;
+        this.rango = Rango.ALUMNO_PILOTO;
     }
 
     public String getNumeroLicencia() {
@@ -33,12 +34,58 @@ public class Piloto extends Persona {
         this.horasVuelo = horasVuelo;
     }
 
-    public String getRango() {
+    public Rango getRango() {
         return rango;
     }
 
-    public void setRango(String rango) {
+    public void setRango(Rango rango) {
         this.rango = rango;
+    }
+
+    /**
+     * @see "Setea el rango del piloto de acuerdo a sus horas de vuelo"
+     */
+    public void actualizarRango () {
+        if (this.horasVuelo >= 500)
+        {
+            setRango(Rango.COMANDANTE);
+        }
+        else if (this.horasVuelo >= 400)
+        {
+            setRango(Rango.SEGUNDO_COMANDANTE);
+        }
+        else if (this.horasVuelo >= 300)
+        {
+            setRango(Rango.TERCER_COMANDANTE);
+        }
+        else if (this.horasVuelo >= 200)
+        {
+            setRango(Rango.CAPITAN);
+        }
+        else if (this.horasVuelo >= 150)
+        {
+            setRango(Rango.SEGUNDO_CAPITAN);
+        }
+        else if (this.horasVuelo >= 100)
+        {
+            setRango(Rango.TERCER_CAPITAN);
+        }
+        else if (this.horasVuelo >= 80)
+        {
+            setRango(Rango.PRIMER_OFICIAL);
+        }
+        else if (this.horasVuelo >= 50)
+        {
+            setRango(Rango.SEGUNDO_OFICIAL);
+        }
+        else if (this.horasVuelo >= 30)
+        {
+            setRango(Rango.PILOTO_INSTRUMENTAL);
+        }
+        else if (this.horasVuelo < 30)
+        {
+            setRango(Rango.ALUMNO_PILOTO);
+        }
     }
 
     @Override
